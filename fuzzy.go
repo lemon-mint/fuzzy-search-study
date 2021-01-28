5dee969db20dfa74fdd5fc6e4c1e38c07265ff89
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2021-01-28 16:37:05 +0900

    strictScore : negative on  miss match

--- fuzzy.go
+++ fuzzy.go
@@ -71,6 +71,52 @@ func score(src, query string) int {
 	return matchScore
 }
 
+func strictScore(src, query string) int {
+	matchScore := 0
+	if len(query) > len(src) {
+		return matchScore
+	}
+	cur := 0
+	lastword := ""
+	wordScore := 0
+	for i := range query {
+		if src == query {
+			matchScore += len(src) * 200
+		}
+		for j := cur; j < len(src); j++ {
+			if src[j] == query[i] {
+				if j-cur == 1 {
+					lastword += src[j : j+1]
+					wordScore += 20
+				} else {
+					lastword = src[j : j+1]
+					matchScore += wordScore
+					wordScore = 0
+				}
+				cur = j
+				matchScore += 100
+				break
+			} else if strings.ToLower(src[j:j+1]) == strings.ToLower(query[i:i+1]) {
+				if j-cur == 1 {
+					lastword += src[j : j+1]
+					wordScore += 10
+				} else {
+					lastword = src[j : j+1]
+					matchScore += wordScore
+					wordScore = 0
+				}
+				cur = j
+				matchScore += 50
+				break
+			}
+		}
+	}
+	if !match(src, query) {
+		return -1*(len(src)*200*len(query)) + matchScore
+	}
+	return matchScore
+}
+
 type fuzzySorter struct {
 	data []string
 	key  string
